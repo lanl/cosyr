7c1f355f513572b5cf1a560e1247bf197167bff4
Hoby Rakotoarivelo <hoby@lanl.gov>
2021-04-13 19:48:28 -0600

    Add script to test compilation within container

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+docker pull hobywan/cosyr
+docker ps
+docker run -v "$(pwd):/home/cosyr" -it hobywan/cosyr /bin/bash -c \
+      "cd /home/cosyr && \
+      mkdir build-test &&
+      cd build-test && \
+      cmake -Wno-dev \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DENABLE_TCMALLOC=False \
+        -DPORTAGE_DIR=/home/dependencies/portage \
+        -DKokkos_DIR=/home/dependencies/kokkos \
+        -Dpybind11_DIR=/home/dependencies/pybind \
+        -DCabana_DIR=/home/dependencies/cabana \
+        .. && \
+      make -j 4"
