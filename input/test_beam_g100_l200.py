3e5798d232eb2c580d9c049c9b86684aa41b0d82
Chengkun Huang <huangck@lanl.gov>
2021-08-26 13:44:23 -0700

    update g=100 round beam benchmark

--- input/test_beam_g100_l200.py
+++ input/test_beam_g100_l200.py
@@ -8,7 +8,8 @@ from input.misc import *
 
 ####################### Preprocessing ##########################
 
-run_name = "test_beam_g100_l200"
+run_name = "test_beam_g100_l200_d200_dyn_sub_psi0.01_1e5steps_3000x50_10Mpart"
+
 
 ## electron and trajectory
 gamma=100
@@ -30,9 +31,10 @@ if (mpi_rank==0) : print("npt_alpha={}, npt_chi={}".format(npt_alpha, npt_chi))
 ## wavelet emission
 num_wavefronts = 3000              # number of wavefronts
 num_dirs = 50                      # number of field lines
-num_step = num_wavefronts          # number of steps (currently always equal to num_wavefronts)
+num_step = 100000                  # number of steps (currently always equal to num_wavefronts)
 dt = psi_max/num_step              # time step in electron rest frame
-emission_interval = 1              # emission record interval 
+emission_interval = 30 #num_step-1     # only emit wavefronts at simulation end (test purpose)
+emission_start = num_step-1 - (num_wavefronts-1)*emission_interval 
 
 ## remap
 remap_interval = num_step-1        # interval of doing remapping (in time steps)
@@ -50,6 +52,7 @@ parameters[0] = gamma              # central energy for all types
 parameters[1] = 100.0              # propagation angle for type 1, radius (cm) for type 2, frequency for type 3
 
 beam = init_beam(num_particles, gamma, lbeam, dbeam, mpi_rank)
+beam[:,0] = beam[:,0] - 0.6*lbeam*1e-6
 #beam = generate_microbunches(overall_beam_env='gaussian', _npart=num_particles, 
 #                _nbunches=1, _sgmx_sub_div=6.0, _lbeam=lbeam, _dbeam=dbeam, _mpi_rank=mpi_rank)
 # del beam     # init a single particle instead
@@ -61,14 +64,18 @@ mesh_span_angle = scaled_alpha/gamma**3   # in radians
 mesh_width = scaled_chi/gamma**2          # in unit of radius
 
 # load wavelets
+load_subcycle_wavelets = True 
 cosyr_root = '..'
-path2subcycling = cosyr_root + "/input/wavelets/g100-200x200um-sub"
-wavelet_x, wavelet_y, wavelet_field = load_wavelets(path2subcycling, fld_file="EBRad_sub.csv", unscale_coord=True, _gamma = gamma)
-if (mpi_rank==0) :
-    print("wavelet shape =", wavelet_x.shape, wavelet_y.shape, wavelet_field.shape)
-    print("wavelet field 0 min/max =", wavelet_field[0,:].min(), wavelet_field[0,:].max())
-    print("wavelet field 1 min/max =", wavelet_field[1,:].min(), wavelet_field[1,:].max())
-    print("wavelet field 2 min/max =", wavelet_field[2,:].min(), wavelet_field[2,:].max())
+path2subcycling = cosyr_root + "/input/wavelets/g100-200x200um-all_3000x50"
+if (load_subcycle_wavelets) :
+    wavelet_x, wavelet_y, wavelet_field = load_wavelets(path2subcycling, X_coord_file="scaled_xprime_all.npy",
+        Y_coord_file="scaled_yprime_all.npy", fld_file="EBRad_all.npy", psi_file="Psi_all.npy", unscale_coord=True, 
+        _gamma = gamma, broadcast=True, filter=True, filter_exp="np.where((psi<0.01) & (wx>-200.0/1e6))")
+    if (mpi_rank==0) :
+        print("wavelet shape =", wavelet_x.shape, wavelet_y.shape, wavelet_field.shape)
+        print("wavelet field 0 min/max =", wavelet_field[0,:].min(), wavelet_field[0,:].max())
+        print("wavelet field 1 min/max =", wavelet_field[1,:].min(), wavelet_field[1,:].max())
+        print("wavelet field 2 min/max =", wavelet_field[2,:].min(), wavelet_field[2,:].max())
 
 min_emit_angle = 0.0 
 
